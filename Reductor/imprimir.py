b6bebb8fd33603ad5ffc075228351b9f9ca284a0
juanpablo <juan.pablo.rendon@correounivalle.edu.co>
2020-10-20 12:28:15 -0500

    second

--- Reductor/imprimir.py
+++ Reductor/imprimir.py
@@ -1,11 +1,12 @@
 
 
 def imprimir(xsat, clausulas, variables):
-    print 'p cnf ' + str(variables) + ' ' + str(clausulas)
-    out = ''
-    for i in range(clausulas):
-        for j in range(len(xsat[i])):
-            out += str(xsat[i][j]) + ' '
-        out += str(0)
-        out += '\n'
-    print out
+	print("p cnf "+str(variables)+" "+str(clausulas))
+	out = ""
+	for i in range(clausulas):
+		for j in range(len(xsat[i])):
+			out += str(xsat[i][j]) + " "
+		out += str(0)
+		out += "\n"
+	print(out)
+
